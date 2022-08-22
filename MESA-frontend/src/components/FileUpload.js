053d309d0bef7f791434138c5f575c406d956e48
hetvi1908 <hetvi1908@gmail.com>
2022-08-23 03:29:47 +0530

    --ocr pdf reading done

---
+++ MESA-frontend/src/components/FileUpload.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import axios from 'axios';
+
+function FileUpload({ uploadFile, setFile, setFileName }) {
+
+
+
+    const saveFile = (e) => {
+        setFile(e.target.files[0]);
+        setFileName(e.target.files[0].name);
+    };
+
+
+
+    return (
+        <div className="App">
+            <input type="file" onChange={saveFile} />
+            <button onClick={uploadFile}>Upload</button>
+        </div>
+    );
+}
+
+
+export default FileUpload;
