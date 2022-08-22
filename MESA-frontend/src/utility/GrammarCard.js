9fec27e82fda887d334d71408de90eb2ce6df6c6
JayThadeshwar <jaythadeshwar007@gmail.com>
2022-08-22 12:42:39 +0530

    Color sentence in grammer

--- MESA-frontend/src/utility/GrammarCard.js
+++ MESA-frontend/src/utility/GrammarCard.js
@@ -1,20 +1,83 @@
 import React from "react";
 
 function GmCard(sent, nouns, adjective, verb, conjunction, adposition) {
+  
+  sent = sent.replace(/(\r\n|\n|\r)/gm, " ");
+  const words = sent.split(" ");
+  
+  sent = sent.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  const puctLessWords = sent.split(" ");
+
+  let nounsList = []
+  if(nouns !== undefined) {
+    nouns.forEach((element) => {
+      nounsList.push(element["word"]);
+    });
+  }
+
+  let adjectiveList = []
+  if(adjective !== undefined) {
+    adjective.forEach((element) => {
+      adjectiveList.push(element["word"]);
+    });
+  }
+
+  let verbList = []
+  if(verb !== undefined) {
+    verb.forEach((element) => {
+      verbList.push(element["word"]);
+    });
+  }
+
+  let conjunctionList = []
+  if(conjunction !== undefined) {
+    conjunction.forEach((element) => {
+      conjunctionList.push(element["word"]);
+    });
+  }
+
   return (
     <table style={{ marginBottom: "1%" }}>
       <tr>
         <th>Sentence:</th>
-        <td>{sent}</td>
+        <td>
+          {words.map((element,id) => {            
+            let styles = {};
+            console.log(puctLessWords[id] +" "+element+" "+nounsList.includes(puctLessWords[id]));
+            if(nounsList.includes(puctLessWords[id])){
+              styles = {
+                color: 'red'
+              };                 
+            } else if(adjectiveList.includes(puctLessWords[id])){
+              styles = {
+                color: 'green'
+              };
+            } else if(verbList.includes(puctLessWords[id])){
+              styles = {
+                color: 'purple'
+              };
+            } else if(conjunctionList.includes(puctLessWords[id])){
+              styles = {
+                color: 'blue'
+              };
+            }
+            element = element.toString() + " ";
+            return (
+              <span key={id} style={styles}>
+                {element}
+              </span>
+            );
+          })}
+        </td>
       </tr>
       {nouns === undefined ? (
         <></>
       ) : (
         <tr>
           <th>Noun(संज्ञा):</th>
-          {nouns.map((info) => {
+          {nouns.map((info, id) => {
             return (
-              <td>
+              <td key={id}>
                 {info["word"]} ({info["tagExp"]})
               </td>
             );
@@ -26,9 +89,9 @@ function GmCard(sent, nouns, adjective, verb, conjunction, adposition) {
       ) : (
         <tr>
           <th>Adjective(विशेषण):</th>
-          {adjective.map((info) => {
+          {adjective.map((info,id) => {
             return (
-              <td>
+              <td key={id}>
                 {info["word"]} ({info["tagExp"]})
               </td>
             );
@@ -40,9 +103,9 @@ function GmCard(sent, nouns, adjective, verb, conjunction, adposition) {
       ) : (
         <tr>
           <th>Verb(क्रिया):</th>
-          {verb.map((info) => {
+          {verb.map((info,id) => {
             return (
-              <td>
+              <td key={id}>
                 {info["word"]} ({info["tagExp"]})
               </td>
             );
@@ -54,9 +117,9 @@ function GmCard(sent, nouns, adjective, verb, conjunction, adposition) {
       ) : (
         <tr>
           <th>Conjunction(संयोजक):</th>
-          {conjunction.map((info) => {
+          {conjunction.map((info, id) => {
             return (
-              <td>
+              <td key={id}>
                 {info["word"]} ({info["tagExp"]})
               </td>
             );
@@ -68,9 +131,9 @@ function GmCard(sent, nouns, adjective, verb, conjunction, adposition) {
       ) : (
         <tr>
           <th>Adposition(अनुस्थापन):</th>
-          {adposition.map((info) => {
+          {adposition.map((info, id) => {
             return (
-              <td>
+              <td key={id}>
                 {info["word"]} ({info["tagExp"]})
               </td>
             );
