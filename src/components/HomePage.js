9155b7707bf1c90ac07378cb1ee6a1dffb5835bc
JayThadeshwar <jaythadeshwar007@gmail.com>
2022-08-25 12:35:14 +0530

    changes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -23,7 +23,7 @@ import GameSection from "./GameSection/GameSection"
 import Grid from '@mui/material/Grid'; // Grid version 1
 import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 
-
+import { useLocation } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   homepage: {
@@ -51,6 +51,8 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function Homepage() {
+  const location = useLocation();
+
   const classes = useStyles();
   const navigate = useNavigate();
 
@@ -64,39 +66,38 @@ function Homepage() {
 
   const fetchChps = async () => {
     setIsChapterLoading(true);
-    console.log(con.BASE_URI + "/chapters");
-    const response = await fetch(con.BASE_URI + "/chapters");
+    const response = await fetch(con.BASE_URI + "/chapters?userId=" + location.state.id);
     const data = await response.json();
-      let carouselItem = data.map((item, key) => {                  
-          
-            <HomePageCard key={key} title={item["name"]} date={""}  img={carouselCardImg} description={item.content} routeLink={""} downloadLink={""} />                            
-      }
-          );
-          setIsChapterLoading(false);
-          setChapterContent(carouselItem);
-    
+    console.log(data)
+    let carouselItem = data.map((item, key) => {
+      return <HomePageCard key={key} title={item["name"]} date={""} img={carouselCardImg} description={item.content} routeLink={""} downloadLink={""} />
+    }
+    );
+    setIsChapterLoading(false);
+    setChapterContent(carouselItem);
+
 
   };
 
   const responsive = {
     superLargeDesktop: {
-        // the naming can be any, depends on you.
-        breakpoint: { max: 4000, min: 3000 },
-        items: 5
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
     },
     desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 3
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
     },
     tablet: {
-        breakpoint: { max: 1024, min: 464 },
-        items: 2
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
     },
     mobile: {
-        breakpoint: { max: 464, min: 0 },
-        items: 1
+      breakpoint: { max: 464, min: 0 },
+      items: 1
     }
-};
+  };
 
   useEffect(() => {
     fetchChps();
@@ -116,51 +117,51 @@ function Homepage() {
 
       <div>
         <center>
-          {/* <img src={head} alt="tp" className={classes.study}></img> */}          
+          {/* <img src={head} alt="tp" className={classes.study}></img> */}
         </center>
       </div>
-      
+
 
       {/* ================================================================== */}
 
       {/* Card Carousel */}
 
       <div className={classes.homepage}>
-        
+
         {/* <Paper elevation={3} style={{}}> */}
-          <Box p={0} className={classes.bluecolor}>
-            <Typography
-              variant="h4"
-              style={{
-                textAlign: "left",
-                color: "black",
-                paddingLeft: 35,
-                fontFamily: "fantasy",
-              }}
-            >
-              CHAPTERS
-            </Typography>
-            <br />
-
-            {isChapterLoading ? (
-              <LoadingSpinner />
-            ) : (
-              
-              // <Carousel cols={6} rows={1} gap={1} loop>
-               
-              <Carousel responsive={responsive}>
-                {
-                  chapterContent                  
-                }            
-              </Carousel>
-                
-              // </Carousel>
-            )}
-            <br />
-          </Box>
-
+        <Box p={0} className={classes.bluecolor}>
+          <Typography
+            variant="h4"
+            style={{
+              textAlign: "left",
+              color: "black",
+              paddingLeft: 35,
+              fontFamily: "fantasy",
+            }}
+          >
+            CHAPTERS
+          </Typography>
           <br />
-          {/* <Grid
+
+          {isChapterLoading ? (
+            <LoadingSpinner />
+          ) : (
+
+            // <Carousel cols={6} rows={1} gap={1} loop>
+
+            <Carousel responsive={responsive}>
+              {
+                chapterContent
+              }
+            </Carousel>
+
+            // </Carousel>
+          )}
+          <br />
+        </Box>
+
+        <br />
+        {/* <Grid
             container
             spacing={3}
             style={{
@@ -180,7 +181,7 @@ function Homepage() {
                 ADD CHAPTER
               </Button>
             </Grid>
-          </Grid> */}        
+          </Grid> */}
 
       </div>
 
@@ -188,8 +189,8 @@ function Homepage() {
 
       {/* Games */}
 
-          <GameSection></GameSection>
-          {/* <Grid container spacing={0}>
+      <GameSection></GameSection>
+      {/* <Grid container spacing={0}>
             <Grid xs={12} s={12} md={6} lg={6} xl={6}>Image</Grid>
             <Grid xs={0} s={0} md={6} lg={6} xl={6}>Description</Grid>
           </Grid>
